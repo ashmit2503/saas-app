9051be88f77dade608a080ae9da15045a072dbf6
Ashmit <ashmit2503@gmail.com>
2025-09-29 14:08:31 +0530

    Updated build args

--- src/app/settings.py
+++ src/app/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config('CONN_MAX_AGE', default=30, cast=int)
-DATABASE_URL = config('DATABASE_URL', cast=str)
+DATABASE_URL = config('DATABASE_URL', default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
